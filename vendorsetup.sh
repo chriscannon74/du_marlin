30d9da370bd8f59c9c2c52bb5ec83db60e1d961d
spezi77 <spezi77@gmx.com>
2018-01-06 11:34:44 +0100

    marlin/sailfish: Enable Always On Display. -Always On Display toggle will be shown in the Display settings. -Screen will blank when going from Ambient to Active to allow for consistent wakes from doze.
    Thanks @AOSPP for figuring it out!
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,6 +27,6 @@
 #  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-add_lunch_combo aosp_marlin-userdebug
+add_lunch_combo du_marlin-userdebug
 add_lunch_combo aosp_marlin_svelte-userdebug
 add_lunch_combo aosp_sailfish-userdebug
